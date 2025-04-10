3b6c70fd1ae441aa66297298e2df41ca3d20941a
Bhavesh Pharate <pharatebhavesh@gmail.com>
2025-04-10 22:31:36 +0530

    bp-14: add file uploaded

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 7777,()=>{
+        console.log("server running on port:",process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.log("Error MongFail",error)
+});
